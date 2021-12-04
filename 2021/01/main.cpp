04895730936cdc53cdcd2e246e710ad511a0fc08
emilsbull <fake@mail.ok>
2021-12-04 06:03:46 -0800

    AOC 01: add dynamic implementation

--- 2021/01/main.cpp
+++ 2021/01/main.cpp
@@ -1,15 +1,41 @@
 #include <iostream>
 #include <vector>
+// #include <experimental/filesystem>
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-int main(int argc, char* argv[])
+namespace fs = std::filesystem;
+
+int main(int , char* [])
 {
-  std::vector<int> measurements {199, 200, 208, 210, 200, 207, 240, 269, 260, 263};
+  const std::string path{"input.txt"};
+  // std::vector<int> measurements {199, 200, 208, 210, 200, 207, 240, 269, 260, 263};
+  std::vector<int> measurements;
+
+  std::ifstream infile(path, std::ios::in);
+  std::string line;
+  while (std::getline(infile, line))
+  {
+      std::istringstream iss(line);
+      int a;
+      if (!(iss >> a)) { break; } // error
+
+      measurements.push_back(a);
+  }
+  infile.close();
+
+  if (measurements.empty()) {
+    std::cerr << "inputfile empty" << std::endl;
+    return -1;
+  }
 
   auto it = measurements.begin();
   auto last = *it++;
   auto timesIncreased = 0;
 
-  for(it; it != measurements.end(); ++it) {
+  for(; it != measurements.end(); ++it) {
     if(last < *it) {
       timesIncreased++;
     } 
@@ -17,5 +43,5 @@ int main(int argc, char* argv[])
   }
 
   std::cout << timesIncreased << std::endl;
-
+  return 0;
 }
