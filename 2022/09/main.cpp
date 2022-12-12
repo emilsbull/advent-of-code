3c5887a95d1f306fba774f95689b27d3f50026f3
emilsbull <47860724+emilsbull@users.noreply.github.com>
2022-12-12 20:31:10 +0000

    AOC22 09b

--- 2022/09/main.cpp
+++ 2022/09/main.cpp
@@ -15,57 +15,85 @@
 
 #include "string_utils.h"
 
+bool isTailMovingX(std::pair<int, int> head, std::pair<int, int> tail)
+{
+  return abs(head.first - tail.first) >= 2;
+}
+
+bool isTailMovingY(std::pair<int, int> head, std::pair<int, int> tail)
+{
+  return abs(head.second - tail.second) >= 2;
+}
 bool isTailMoving(std::pair<int, int> head, std::pair<int, int> tail)
 {
-  return abs(head.first - tail.first) >= 2 || abs(head.second - tail.second) >= 2;
+  return isTailMovingX(head, tail) || isTailMovingY(head, tail);
 }
 
-void moveHead(std::pair<int, int>& head, char dir) {
-      switch (dir)
-      {
-      case 'R':
-        head.first++;
-        break;
-      case 'L':
-        head.first--;
-        break;
-      case 'U':
-        head.second++;
-        break;
-      case 'D':
-        head.second--;
-        break;
-      }
+void moveHead(std::pair<int, int> &head, char dir)
+{
+  switch (dir)
+  {
+  case 'R':
+    head.first++;
+    break;
+  case 'L':
+    head.first--;
+    break;
+  case 'U':
+    head.second++;
+    break;
+  case 'D':
+    head.second--;
+    break;
+  }
 }
 
-void moveTail(std::pair<int, int> head, std::pair<int, int>& tail, char dir) {
-      if (isTailMoving(head, tail))
-      {
-        if (dir == 'R' || dir == 'L')
-        {
-          tail.second = head.second;
-        }
-        else
-        {
-          tail.first = head.first;
-        }
+void moveTail(std::pair<int, int> head, std::pair<int, int> &tail)
+{
+  if (isTailMoving(head, tail))
+  {
 
-        switch (dir)
-        {
-        case 'R':
-          tail.first++;
-          break;
-        case 'L':
-          tail.first--;
-          break;
-        case 'U':
-          tail.second++;
-          break;
-        case 'D':
-          tail.second--;
-          break;
-        }
+    if (isTailMovingX(head, tail))
+    {
+      if (head.first > tail.first)
+      {
+        tail.first++;
       }
+      else
+      {
+        tail.first--;
+      }
+      
+      if (head.second > tail.second)
+      {
+        tail.second++;
+      }
+      else if (head.second < tail.second)
+      {
+        tail.second--;
+      }
+    }
+    else
+    {
+      if (head.second > tail.second)
+      {
+        tail.second++;
+      }
+      else
+      {
+        tail.second--;
+      }
+
+      if (head.first > tail.first)
+      {
+        tail.first++;
+      }
+      else if (head.first < tail.first)
+      {
+        tail.first--;
+      }
+    }
+  }
 }
 
 void puzzleA(std::vector<std::pair<char, int>> motion)
@@ -78,10 +106,10 @@ void puzzleA(std::vector<std::pair<char, int>> motion)
   {
     for (int i = 0; i < count; ++i)
     {
-      auto& head = knots[0];
-      auto& tail = knots[1];
+      auto &head = knots[0];
+      auto &tail = knots[1];
       moveHead(head, dir);
-      moveTail(head, tail, dir);
+      moveTail(head, tail);
       visited[tail] = true;
     }
   }
@@ -91,12 +119,28 @@ void puzzleA(std::vector<std::pair<char, int>> motion)
   std::cout << std::endl;
 }
 
-void puzzleB(std::vector<std::pair<char, int>>)
+void puzzleB(std::vector<std::pair<char, int>> motion)
 {
-  int score = 0;
+  constexpr size_t nKnots = 10;
+  std::array<std::pair<int, int>, nKnots> knots;
+  std::map<std::pair<int, int>, bool> visited;
+  visited[knots[0]] = true;
+
+  for (auto &&[dir, count] : motion)
+  {
+    for (int i = 0; i < count; ++i)
+    {
+      moveHead(knots[0], dir);
+      for (std::size_t j = 1; j < nKnots; ++j)
+      {
+        moveTail(knots[j - 1], knots[j]);
+      }
+      visited[knots.back()] = true;
+    }
+  }
 
   std::cout << "---------------------" << std::endl;
-  std::cout << "output B:" << score;
+  std::cout << "output B:" << visited.size();
   std::cout << std::endl;
 }
 
