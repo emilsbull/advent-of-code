589408dda0cb71323edc4416019ff00a7ed55de3
emilsbull <47860724+emilsbull@users.noreply.github.com>
2023-12-09 12:32:21 +0000

    simplify visitAdiacent

--- utils/array_utils.h
+++ utils/array_utils.h
@@ -6,64 +6,6 @@
 namespace utils
 {
     template <typename C, typename FUN>
-    void visitAdiacent(size_t row, size_t col, const C& container, FUN&& fun)
-    {
-        auto rowMax = container.size();
-        auto colMax = container[row].size();
-
-        if (row > 0) {
-            if (col > 0) {
-                // o|_|_
-                // _|X|_
-                // _|_|_
-                fun(container[row - 1][col - 1]);
-            }
-            // _|o|_
-            // _|X|_
-            // _|_|_
-            fun(container[row - 1][col]);
-            if (col < colMax - 1) {
-                // _|_|o
-                // _|X|_
-                // _|_|_
-                fun(container[row - 1][col + 1]);
-            }
-        }
-
-        if (col > 0) {
-            // _|_|_
-            // o|X|_
-            // _|_|_
-            fun(container[row][col - 1]);
-        }
-
-        if (col < colMax - 1) {
-            // _|_|_
-            // _|X|o
-            // _|_|_
-            fun(container[row][col + 1]);
-        }
-
-        if (row < rowMax - 1) {
-            if (col > 0) {
-                // _|_|_
-                // _|X|_
-                // o|_|_
-                fun(container[row + 1][col - 1]);
-            }
-            // _|_|_
-            // _|X|_
-            // _|o|_
-            fun(container[row + 1][col]);
-            if (col < colMax - 1) {
-                // _|_|_
-                // _|X|_
-                // _|_|o
-                fun(container[row + 1][col + 1]);
-            }
-        }
-    }
-    template <typename C, typename FUN>
     void adiacentIndices(size_t row, size_t col, const C& container, FUN&& fun)
     {
         auto rowMax = container.size();
@@ -120,6 +62,12 @@ namespace utils
                 fun(row + 1, col + 1);
             }
         }
+    }
+
+    template <typename C, typename FUN>
+    void visitAdiacent(size_t row, size_t col, const C& container, FUN&& fun)
+    {
+        adiacentIndices(row, col, container, [&container, &fun](size_t r, size_t c) { fun(container[r][c]); });
     }
 }  // namespace utils
 
