29123968e62a95217a98b03ec64757a5d47b17cf
emilsbull <47860724+emilsbull@users.noreply.github.com>
2024-01-12 13:20:53 +0000

    wip

--- 2023/05/main.cpp
+++ 2023/05/main.cpp
@@ -16,7 +16,8 @@
 #include "map_utils.h"
 #include "string_utils.h"
 
-struct Seed {
+struct Seed
+{
     int64_t seed{0};
     bool mapped{false};
 
@@ -43,12 +44,12 @@ struct Range
         return (v.seed >= start) && (v.seed < start + length);
     }
 
-    bool map(Seed& v)
+    bool map(Seed &v)
     {
         if (contains(v) && !v.mapped) {
-             v.seed = destination + (v.seed - start);
-             v.mapped = true;
-             return true;
+            v.seed = destination + (v.seed - start);
+            v.mapped = true;
+            return true;
         }
         return false;
     }
@@ -70,6 +71,8 @@ std::pair<int, int> parse(const std::vector<std::string> &input)
     // #######
 
     std::vector<Seed> seeds;
+    std::vector<std::vector<Range> > ranges;
+    std::vector<Range> *currentRange = nullptr;
 
     enum class state
     {
@@ -97,29 +100,84 @@ std::pair<int, int> parse(const std::vector<std::string> &input)
             }
             case state::mappingHead: {
                 readingState = state::mapping;
+                ranges.push_back({});
+                currentRange = &ranges.back();
 
-                for (auto &seed : seeds) {
-                    seed.mapped = false;
-                    // std::cout << seed.seed << std::endl;
-                }
-                // std::cout << "----------" << std::endl;
                 break;
             }
             case state::mapping: {
                 std::stringstream ss(line);
                 Range r;
                 ss >> r;
-
-                for (auto &seed : seeds) {
-                    r.map(seed);
-                }
+                currentRange->push_back(r);
                 break;
             }
         }
     }
 
-    auto aIterator = std::min_element(seeds.begin(), seeds.end(), [](const auto &a, const auto& b) -> bool {return a.seed < b.seed;});
+    for (auto &rrange : ranges) {
+        for (auto &r : rrange) {
+            for (auto &seed : seeds) {
+                r.map(seed);
+            }
+        }
+
+        for (auto &seed : seeds) {
+            seed.mapped = false;
+        }
+    }
+
+    auto aIterator = std::min_element(seeds.begin(), seeds.end(),
+                                      [](const auto &a, const auto &b) -> bool { return a.seed < b.seed; });
     sumA = aIterator->seed;
+
+    // ###########################
+
+    std::vector<Seed> minSeeds;
+
+    std::vector<Seed> mSeeds;
+
+    for (auto it = ranges.rbegin(); it != ranges.rend(); ++it) {
+        for (auto &range : *it) {
+            for (auto &seed : mSeeds) {
+                range.map(seed);
+            }
+        }
+        for (auto &seed : mSeeds) {
+            seed.mapped = false;
+        }
+        for (auto &range : *it) {
+            mSeeds.push_back({range.start, false});
+            mSeeds.push_back({range.start + range.length, false});
+        }
+    }
+    std::sort(mSeeds.begin(), mSeeds.end(), [](auto a, auto b) -> bool { return a.seed < b.seed; });
+
+    for (size_t i = 0; i < seeds.size(); i += 2u) {
+        auto seedStart = seeds[i].seed;
+        auto seedsLength = seeds.at(i + 1).seed;
+
+        std::vector<Seed> seedsb(seedsLength);
+
+        for (auto j = 0; j < seedsLength; ++j) {
+            seedsb.push_back(Seed{j + seedStart, false});
+        }
+
+        for (auto &rrange : ranges) {
+            std::for_each(rrange.begin(), rrange.end(), [&seedsb](Range &r) {
+                std::for_each(seedsb.begin(), seedsb.end(), [&r](Seed &seed) { r.map(seed); });
+            });
+
+            std::for_each(seedsb.begin(), seedsb.end(), [](Seed &seed) { seed.mapped = false; });
+        }
+        auto bIterator = std::min_element(seeds.begin(), seeds.end(),
+                                          [](const auto &a, const auto &b) -> bool { return a.seed < b.seed; });
+        minSeeds.push_back(*bIterator);
+    }
+
+    auto bIterator = std::min_element(minSeeds.begin(), minSeeds.end(),
+                                      [](const auto &a, const auto &b) -> bool { return a.seed < b.seed; });
+    sumB = bIterator->seed;
 
     return {sumA, sumB};
 }
@@ -139,7 +197,7 @@ void puzzle(const std::vector<std::string> &commands)
 
 int main(int, char *[])
 {
-    constexpr bool realPuzzle = true;
+    constexpr bool realPuzzle = false;
     const std::string file{(realPuzzle ? "input1.txt" : "input.txt")};
 
     const std::string path{file};
