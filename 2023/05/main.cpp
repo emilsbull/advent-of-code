3167c653d41d0154571cf41c6b577287837515a1
emilsbull <47860724+emilsbull@users.noreply.github.com>
2024-03-01 09:27:31 +0000

    Rewrite 5b

--- 2023/05/main.cpp
+++ 2023/05/main.cpp
@@ -15,143 +15,156 @@
 #include "array_utils.h"
 #include "map_utils.h"
 #include "string_utils.h"
+#include "input_helpers.h"
 
-struct Seed {
-    int64_t seed{0};
-    bool mapped{false};
-
-    friend std::istream &operator>>(std::istream &, Seed &);
-};
-
-std::istream &operator>>(std::istream &s, Seed &r)
+std::vector<int64_t> getSeedsPart1(const std::string& line)
 {
-    s >> r.seed;
-    r.mapped = false;
-    return s;
+    std::vector<int64_t> seeds;
+    std::istringstream iss(line.substr(7));
+
+    int64_t number;
+    while (iss >> number)
+        seeds.push_back(number);
+
+    return seeds;
 }
 
-struct Range
+struct Map
 {
-    int64_t start;
-    int64_t destination;
+    int64_t destinationStart;
+    int64_t sourceStart;
     int64_t length;
 
-    friend std::istream &operator>>(std::istream &, Range &);
-
-    bool contains(Seed v)
+    Map(int64_t destinationStart, int64_t sourceStart, int64_t length)
+        : destinationStart(destinationStart), sourceStart(sourceStart), length(length)
     {
-        return (v.seed >= start) && (v.seed < start + length);
-    }
-
-    bool map(Seed& v)
-    {
-        if (contains(v) && !v.mapped) {
-             v.seed = destination + (v.seed - start);
-             v.mapped = true;
-             return true;
-        }
-        return false;
     }
 };
 
-std::istream &operator>>(std::istream &s, Range &r)
+struct SeedRange
 {
-    s >> r.destination >> r.start >> r.length;
-    return s;
+    int64_t start;
+    int64_t length;
+
+    SeedRange(int64_t start, int64_t length) : start(start), length(length)
+    {
+    }
+};
+
+std::vector<SeedRange> getSeedsPart2(const std::string& line)
+{
+    std::vector<SeedRange> seeds;
+    std::istringstream iss(line.substr(7));
+
+    int64_t start, length;
+    while (iss >> start >> length)
+        seeds.push_back({start, length});
+
+    return seeds;
 }
 
-std::pair<int, int> parse(const std::vector<std::string> &input)
+std::vector<std::vector<Map> > getMaps(const std::vector<std::string>& input)
 {
-    // ##### evaluate
+    std::vector<std::vector<Map> > maps = {{}};  //level 0
 
-    int sumA{0};
-    int sumB{0};
-
-    // #######
-
-    std::vector<Seed> seeds;
-
-    enum class state
-    {
-        seeds,
-        mappingHead,
-        mapping
-    } readingState = state::seeds;
-
-    for (auto &line : input) {
-        if (line.empty()) {
-            readingState = state::mappingHead;
+    for (auto i = 3u; i < input.size(); i++) {  //skip seeds
+        //skip empty lines as well as string description after:
+        if (input[i].empty()) {
+            maps.push_back({});
+            i++;
             continue;
         }
 
-        switch (readingState) {
-            case state::seeds: {
-                auto seedsList = utils::splitString(line, ":");
-                std::stringstream ss(seedsList.at(1));
-                Seed seed;
-                while (ss >> seed) {
-                    seeds.push_back(seed);
-                }
-                readingState = state::mappingHead;
-                break;
-            }
-            case state::mappingHead: {
-                readingState = state::mapping;
-
-                for (auto &seed : seeds) {
-                    seed.mapped = false;
-                    // std::cout << seed.seed << std::endl;
-                }
-                // std::cout << "----------" << std::endl;
-                break;
-            }
-            case state::mapping: {
-                std::stringstream ss(line);
-                Range r;
-                ss >> r;
-
-                for (auto &seed : seeds) {
-                    r.map(seed);
-                }
-                break;
-            }
-        }
+        std::istringstream iss(input[i]);
+        int64_t destinationStart, sourceStart, length;
+        iss >> destinationStart >> sourceStart >> length;
+        maps.at(maps.size() - 1).push_back({Map(destinationStart, sourceStart, length)});
     }
-
-    auto aIterator = std::min_element(seeds.begin(), seeds.end(), [](const auto &a, const auto& b) -> bool {return a.seed < b.seed;});
-    sumA = aIterator->seed;
-
-    return {sumA, sumB};
+    return maps;
 }
 
-void puzzle(const std::vector<std::string> &commands)
+int main()
 {
-    auto [resultA, resultB] = parse(commands);
+    int64_t part1 = 0;
+    int64_t part2 = 0;
 
-    std::cout << "---------------------" << std::endl;
-    std::cout << "output A:" << resultA;
-    std::cout << std::endl;
-
-    std::cout << "---------------------" << std::endl;
-    std::cout << "output b:" << resultB;
-    std::cout << std::endl;
-}
-
-int main(int, char *[])
-{
     constexpr bool realPuzzle = true;
     const std::string file{(realPuzzle ? "input1.txt" : "input.txt")};
 
-    const std::string path{file};
-    std::ifstream infile(path, std::ios::in);
-    std::string line;
+    std::vector<std::string> input = utils::getInput(file);
 
-    std::vector<std::string> commands;
+    std::vector<std::vector<Map> > maps = getMaps(input);
 
-    while (std::getline(infile, line)) {
-        commands.push_back(line);
+    //Part 1:
+    std::vector<int64_t> seedsPart1 = getSeedsPart1(input[0]);
+    for (auto level : maps) {
+        for (auto& seed : seedsPart1) {
+            for (auto map : level) {
+                if (seed >= map.sourceStart && seed < map.sourceStart + map.length) {
+                    seed = map.destinationStart + (seed - map.sourceStart);
+                    break;
+                }
+            }
+        }
     }
+    part1 = *std::min_element(seedsPart1.begin(), seedsPart1.end());
 
-    puzzle(commands);
-    return 0;
+    //Part 2:
+    std::vector<SeedRange> seedsPart2 = getSeedsPart2(input[0]);
+    for (auto level : maps) {
+        for (auto i = 0u; i < seedsPart2.size(); i++) {
+            for (auto map : level) {
+                int64_t seedStart = seedsPart2[i].start;
+                int64_t seedEnd = seedsPart2[i].start + seedsPart2[i].length;
+                //seedRange is contained in mapRange, map whole:
+                if (seedStart >= map.sourceStart && seedEnd <= map.sourceStart + map.length) {
+                    seedsPart2[i].start = map.destinationStart + (seedStart - map.sourceStart);
+                    break;
+                }
+                //mapRange is contained in seedRange, map contained part:
+                else if (map.sourceStart >= seedStart && map.sourceStart + map.length <= seedEnd) {
+                    //Split the seedRange:
+                    int64_t leftSideLength = map.sourceStart - seedStart;
+                    int64_t rightSideLength = seedEnd - (map.sourceStart + map.length);
+                    //Add left side as new seedRange:
+                    seedsPart2.push_back({seedStart, leftSideLength});
+                    //Add right side as new seedRange:
+                    seedsPart2.push_back({map.sourceStart + map.length, rightSideLength});
+                    //Map middle:
+                    seedsPart2[i].start = map.destinationStart;
+                    seedsPart2[i].length = map.length;
+                    break;
+                }
+                //seedStart is in range, seedEnd is not, map first half:
+                else if (seedStart >= map.sourceStart && seedStart < map.sourceStart + map.length) {
+                    int64_t firstHalfLength = map.sourceStart + map.length - seedStart;
+                    int64_t secondHalfLength = seedsPart2[i].length - firstHalfLength;
+                    //Add second half as new seedRange
+                    seedsPart2.push_back({seedStart + firstHalfLength, secondHalfLength});
+                    //Map first half:
+                    seedsPart2[i].start = seedStart + map.destinationStart - map.sourceStart;
+                    seedsPart2[i].length = firstHalfLength;
+                    break;
+                }
+                //seedEnd is in range, seedStart is not, map second half:
+                else if (seedEnd > map.sourceStart && seedEnd < map.sourceStart + map.length) {
+                    //Split the range:
+                    int64_t firstHalfLength = map.sourceStart - seedStart;
+                    int64_t secondHalfLength = seedsPart2[i].length - firstHalfLength;
+                    //Add first half as new seedRange:
+                    seedsPart2.push_back({seedStart, firstHalfLength});
+                    //Map second half:
+                    seedsPart2[i].start = map.destinationStart;
+                    seedsPart2[i].length = secondHalfLength;
+                    break;
+                }
+            }
+        }
+    }
+    part2 = (*std::min_element(seedsPart2.begin(), seedsPart2.end(), [](const SeedRange& a, const SeedRange& b) {
+                return a.start < b.start;
+            })).start;
+
+    std::cout << "Part 1: " << part1 << std::endl;  //600279879
+    std::cout << "Part 2: " << part2 << std::endl;  //20191102
 }
---
+++ utils/input_helpers.h
@@ -0,0 +1,65 @@
+#include <vector>
+#include <cstdint>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+namespace utils {
+    
+template<typename T>
+std::vector<T> getInput(std::string inputPath) {
+  std::vector<T> data;
+  std::ifstream infile(inputPath);
+  std::string line;
+
+  if(infile.is_open()) {
+    while(getline(infile, line)) {
+      T element;
+      std::stringstream input(line);
+      while(input >> element)
+        data.push_back(element);
+    }
+    infile.close();
+  } else {
+    std::cout << "Unable to open file" << std::endl;
+  }
+
+  return data;
+}
+
+std::vector<std::string> getInput(std::string inputPath) {
+  std::vector<std::string> data;
+  std::ifstream infile(inputPath);
+  std::string line;
+
+  if(infile.is_open()) {
+    while(getline(infile, line)) {
+      data.push_back(line);
+    }
+    infile.close();
+  } else {
+    std::cout << "Unable to open file" << std::endl;
+  }
+
+  return data;
+}
+
+std::vector<std::vector<char>> getInputGrid(std::string inputPath) {
+  std::vector<std::vector<char>> data;
+  std::ifstream infile(inputPath);
+  std::string line;
+
+  if(infile.is_open()) {
+    while(getline(infile, line)) {
+      std::vector<char> row(line.begin(), line.end());
+      data.push_back(row);
+    }
+    infile.close();
+  } else {
+    std::cout << "Unable to open file" << std::endl;
+  }
+
+  return data;
+}
+
+}
